5020ad4d0ad05dcb7f561f0cb0370eff1ceec5ce
DESKTOP-M19AS7U\maricel <chryslee18@gmail.com>
2022-10-12 20:00:04 +0800

    activity 1

---
+++ LeoCharles_Quibuyen/Activity1/script.js
@@ -0,0 +1,22 @@
+var username = charles
+var password = Pass123
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener(`click`, (e) => {
+    e.preventDefault();
+    const username = loginForm === charles;
+    const password = loginForm === Pass123;
+    
+    function username() {
+
+    }
+    function 
+    if (username === `charles` && password === `Pass123`) {
+        alert("You have successfully logged in.");
+    } else {
+        loginErrorMsg.style.opacity = 10;
+    }
+})
