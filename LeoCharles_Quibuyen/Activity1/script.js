aefbfeb66e947044cb85edd3bd0bb6964816a2c0
DESKTOP-M19AS7U\maricel <chryslee18@gmail.com>
2022-10-12 20:02:36 +0800

    activity 1

--- LeoCharles_Quibuyen/Activity1/script.js
+++ LeoCharles_Quibuyen/Activity1/script.js
@@ -1,22 +1,15 @@
-var username = charles
-var password = Pass123
-
-const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
-
-loginButton.addEventListener(`click`, (e) => {
-    e.preventDefault();
-    const username = loginForm === charles;
-    const password = loginForm === Pass123;
-    
-    function username() {
-
+function validate()
+{
+    var username=document.getElementById(`charles`).value;
+    var password=document.getElementById(`Pass123`).value;
+    if(username=="charles"&& password=="Pass123")
+    {
+    alert("Login Successfully");
+    return false;
     }
-    function 
-    if (username === `charles` && password === `Pass123`) {
-        alert("You have successfully logged in.");
-    } else {
-        loginErrorMsg.style.opacity = 10;
+
+    else
+    {
+    alert("Invalid Credentials");
     }
-})
+}
