18bbf3c438fa3c7e3825a2a47ff97411e8e8b337
bryansuello <bryansuello@gmail.com>
2022-11-10 02:38:29 +0800

    php activity - calculate

--- Bryan_Suello/php/php2.php
+++ Bryan_Suello/php/php2.php
@@ -1,4 +1,3 @@
-
 <!-- Create a function to calculate 2 given numbers based on the arithmetic operators given
 Scenarios
 1. If operator is "+" add the 2 numbers print the sum
@@ -7,13 +6,28 @@ Scenarios
 4. If operator is "/" divide the 2 number print the quotient
 5. If operator is none of the above print "Operation not found" -->
 
+// <?php
+    // function calculate($num1, $num2, $operator){
+    //     if($operator === '+') return $num1 + $num2;
+    //     else if($operator === '-') return $num1 - $num2;
+    //     else if($operator === '*') return $num1 * $num2;
+    //     else if($operator ==='/') return $num1 / $num2;
+    //     else return 'Operation not found';
+    // }
+    // echo calculate(8, 8, "*");
+    // 
+    ?>
+
 <?php
-function calculate($num1, $num2, $operator){
-    if($operator === '+') return $num1 + $num2;
-    else if($operator === '-') return $num1 - $num2;
-    else if($operator === '*') return $num1 * $num2;
-    else if($operator ==='/') return $num1 / $num2;
-    else return 'Operation not found';
+function calculate($num1, $num2, $operator)
+{
+  $res = 0;
+  if ($operator === '+') $res = $num1 + $num2;
+  else if ($operator === '-') $res = $num1 - $num2;
+  else if ($operator === '*') $res = $num1 * $num2;
+  else if ($operator === '/') $res = $num1 / $num2;
+  else $res = 'Operation not found.';
+  return $res;
 }
 echo calculate(8, 8, "*");
 ?>
