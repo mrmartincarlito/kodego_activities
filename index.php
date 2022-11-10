ff6b33de42895fa4bc009400be7d0bff53c8ab91
normandayto <nrdayto@gmail.com>
2022-11-10 21:33:47 +0800

    second commit

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
 <?php
 
 function oddNumber ($even, $odd) {
@@ -22,3 +32,6 @@ function oddNumber ($even, $odd) {
 }
 
 oddNumber(20, 20);
+
+</body>
+</html>
