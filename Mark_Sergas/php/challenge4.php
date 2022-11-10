193e74738ce74e7c5c4239dee2404f3ec146c6fe
mark4sure <markgyverangelosergas@gmail.com>
2022-11-11 03:50:23 +0800

    challenge4_update

---
+++ Mark_Sergas/php/challenge4.php
@@ -0,0 +1,11 @@
+<?php
+$n = "10";
+for ($i = 1; $i <= $n; $i++) {
+    if (($i % 2) != 0) {
+        $countOdd = $countOdd++;
+        echo "total odd number is: $countOdd ";
+    else {  
+        countOdd = countOdd + 1;  
+    }
+}
+?>
