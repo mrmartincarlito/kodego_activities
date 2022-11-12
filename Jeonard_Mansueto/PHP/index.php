c8e43e67a3ab42e6fb5709482fdc9f435aeab4d0
saviench <jeonardmansueto.pmi@gmail.com>
2022-11-12 20:40:00 +0800

    activity challenge 5

--- Jeonard_Mansueto/PHP/index.php
+++ Jeonard_Mansueto/PHP/index.php
@@ -1,147 +1,19 @@
 <?php
-
-
-checkUserpass("jeonards","saviench123");
-
-function checkUserpass($user,$password) {
-    $defaultuser="jeonard";
-    $defaultpassword="saviench123";
-
-    if ($user===$defaultuser && $password===$defaultpassword){
-    echo "Succesfully login ". ucfirst($user). " Mansueto";
-} 
-elseif ($user===$defaultuser && $password!==$defaultpassword){
-    echo "Password does not match! ";
-    errorpass( "Remember your Password");
-
-}
-    elseif ($user!==$defaultuser && $password===$defaultpassword){
-      echo "username does not match! ";
-      greethello("remember your username");
-}
-else {
-    echo "user and password dos not exist ";
-    signup(" Signup as Guest");
- 
-}
+$total=0;
+$Sales = array(
+    array("qty"=>2,"desc"=>"item1","amt"=>100),
+    array("qty"=>7,"desc"=>"item2","amt"=>35),
+    array("qty"=>1,"desc"=>"item3","amt"=>350),
+    array("qty"=>2,"desc"=>"item4","amt"=>20)
+);
+echo "qty desc amt total"."<br>";
+echo "--------------------------------------"."<br>";
+foreach ($Sales  as $Sale => $value) {
+    echo  $value["qty"]." \t". $value["desc"]." \t". $value["amt"]." \t". $value["qty"] * $value["amt"]."<br>";
+    $total= $total+ $value["qty"] * $value["amt"];
 }
 
-function greetHello($name){
-    echo "Please ".ucfirst($name);
-}
-
-function signup($guest){
-    echo "Hello ".lcfirst($guest);
-}
-function errorpass($error){
-    echo "Think ".lcfirst($error);
-}
-
-
-echo "<br>";
-
-$num1 = 8;
-$num2 = 4;
-operation(8,4,"+");
-function operation($num1,$num2,$operation){
-$a= $num1 +  $num2;
-$s= $num1 -  $num2;
-$m= $num1 *  $num2;
-$d= $num1 /  $num2;
-
-if($operation=="+"){
-echo " Sum is ". $a;
-}
-elseif ($operation=="-"){
-    echo " Difference is ". $s;
-}
-elseif ($operation=="*"){
-    echo " Product is ". $m;
-}
-elseif ($operation=="-"){
-    echo " Quotient is ". $d;
-}
-else{
-    echo " Operation not found";
-}
-
-}
-
-
-echo "<br>";
-
-$x=3;
-for ($i=0; $i<5; $i++ ) {
- $x = $x * $i;
-    
-}
-echo $x;
-
-
-echo "<br>";
-
-$x=100;
-for($i=0;$i<5;$i++ ){
-    if($i==0) {
-        continue;
-    }
-    $x=$x/$i;
-}
-echo "Quotient is: $x";
-
-
-echo "<br>";
-
-
-even(20);
-function even($x){
-    for($i=1; $i<= $x; $i++) {
-    if ($i % 2 == 0){
-     echo "$i <br>" ;   }
-}
-}
-
-echo "<br>";
-
-odd(16);
-function odd($x){
-    for($i=1; $i<= $x; $i++) {
-    if ($i % 2 != 0){
-     echo "$i <br>" ;   }
-}
-}
-
-
-echo "<br>";
-
-
-$x=1000;
-addevenodd($x);
-function addevenodd($x) {
-    $even=0;
-    $odd=0;
-    for($i=1; $i<=$x; $i++) {
-        if ($i % 2 == 0){
-         $even=$even + $i ;   }
-    
-         else {
-            $odd=$odd + $i;
-         }
-    }
-    echo "sum of all even: ". number_format($even, );
-    echo "<br>";
-    echo "sum of all odd: ". number_format($odd, );
-
-}
-    
-
-
-
-
-
-
-
-
-
+echo "--------------------------------------"."<br>";
+echo "Overall Total:" .$total;
 
 ?>
