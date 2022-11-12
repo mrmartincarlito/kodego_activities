a16d70a0c5fbd90fca3b6d527048c92cae314a4b
DESKTOP-GVVRNI5\STUDY <abaleskent1996@gmail.com>
2022-11-12 23:10:15 +0800

    challenge#1

--- Kent_Goayan/PHP_Prctice/coding_challenge_#1.php
+++
@@ -1,9 +0,0 @@
-<?php
-checkifusernameandpasswordcorrect("admin","password123");
-function checkifusernameandpasswordcorrect ( $username , $password);
-$defaultusername = "admin";
-$defaultpassword = "Password123";
-echo "Successfully Logged In"
-
-
-?>
