8dcfb9b20d36b38f70ae6e47841c65c46e877fda
rommelmercado1311@gmail.com <rommelmercado1311@gmail.com>
2022-11-17 01:49:04 +0800

    final code for Activity 5

--- rommel_mercado/Activity 5/index.php
+++ rommel_mercado/Activity 5/index.php
@@ -1,15 +1,38 @@
 <?php
-	
+
 function presidentsPhilippines(){
+  
+$random = array(
+    "gLorIa mAcaPagal ArrOyo",
+    "rOdrigo rOa Duterte",
+    "benIgno sImeOn aQuino",
+    "bOng Go",
+    "lEni RobredO",
+    "fiDel v Ramos",
+    "roBert seNya",
+    "fErdinanD mArcOs Jr."); 
+  
+$presidents = array(
+    "gLorIa mAcaPagal ArrOyo",
+    "rOdrigo rOa Duterte",
+    "benIgno sImeOn aQuino",
+    "fiDel v Ramos",
+    "fErdinanD mArcOs Jr.",
+    "Rommel Mercado test");
 
-$presidents =array("Gloria Macapagal Arroyo", "Rodrigo Roa Duterte", "Benigno Simeon Aquino III", "Fidel V Ramos", "Ferdinand Marcos Jr.");
-$arraylength = count($presidents);
-
-for($i= 0; $i < $arraylength; $i++) {
-  echo ucwords($presidents[$i]);
-  echo "<br>";
-  }
+for($i=0; $i<count($presidents); $i++){
+  
+  for($x=0; $x<count($random); $x++){
+    
+  if(strtolower($presidents[$i])===strtolower($random[$x])){
+    
+    echo ucwords(strtolower($presidents[$i]));
+    echo "\n";
 }
+}
+}
+}
+
 presidentsPhilippines();
 
 
