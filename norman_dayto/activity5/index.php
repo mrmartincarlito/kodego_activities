632d11f4f15f2afd7ff98a75f6ccac3d2d459be0
normandayto <nrdayto@gmail.com>
2022-11-17 18:02:56 +0800

    Activity 5 Final Commit

--- norman_dayto/activity5/index.php
+++ norman_dayto/activity5/index.php
@@ -1,5 +1,30 @@
 <?php
 
-  
+$namesAll = array (
+  "gLorIa mAcaPagal ArrOyo",
+  "rOdrigo rOa Duterte",
+  "benIgno sImeOn aQuino III",
+  "bOng Go",
+  "lEni RobredO",
+  "fiDel v Ramos",
+  "roBert seNya",
+  "fErdinanD mArcOs Jr."
+);
 
+$namesPresidents = array (
+  "gLorIa mAcaPagal ArrOyo",
+  "rOdrigo rOa Duterte",
+  "benIgno sImeOn aQuino III",
+  "fiDel v Ramos",
+  "fErdinanD mArcOs Jr."
+);
+  
+foreach ($namesAll as $nameAll) {
+  foreach ($namesPresidents as $namePresident) {
+    if ($nameAll == $namePresident) {
+      $nameAll = strtolower($nameAll);
+      echo ucwords($nameAll) . "\n";
+    }
+  }
+}
 ?>
