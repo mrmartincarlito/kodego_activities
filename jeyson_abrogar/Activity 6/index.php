cf82d9c2802dbc30fd6e0733fcd281d9da5f4955
JEY\Jey <jeysonabrogar.info@gmail.com>
2022-11-20 09:58:03 +0800

    Feature/activity_6_Jey_Abrogar

--- jeyson_abrogar/Activity 6/index.php
+++ jeyson_abrogar/Activity 6/index.php
@@ -4,13 +4,14 @@
      "PRODUCT1" => array(
                   "item" => "Rice",
                   "qty-type" => "-KG",
-                  "qty" => 9,
-                  "price" => 50,
+                  "qty" => 10,
+                  "price" => 50.000,
       ),
+      
      "PRODUCT2" => array(
                   "item" => "Onion",
                   "qty-type" => "-KG",
-                  "qty" => 7,
+                  "qty" => 9,
                   "price" => 10,
       ),
       "PRODUCT3" => array(
@@ -34,7 +35,7 @@
       "PRODUCT6" => array(
                   "item" => "Sugar",
                   "qty-type" => "-KG",
-                  "qty" => 2,
+                  "qty" => 10,
                   "price" => 90,
       ),
       
@@ -47,51 +48,75 @@
     echo "--------------------------------------------------------------\n";
     echo "ITEM             QTY            PRICE           AMOUNT      \n \n";
     
+  $riceDiscountPercentage = 0.10;
+  $onionDiscountPercentage = 0.20;
+  $riceDiscount = 0;
+  $onionDiscount = 0;
+  
   $grandTotal = 0;
-  $discountPercentage = 0.20;
   $subTotal = 0;
   $priceTotal = 0;
-  $discount = 0;
+  $sum = 0;
+
+  $riceDiscount = 0;
+  $onionDiscount = 0;
+
+  
+  $riceTotalAmount = 0;
+  $riceTotal = 0;
+  
   
     foreach ($products as $product){
       
       $priceTotal = $product["qty"] * $product["price"];
+      $riceTotal =  $products ["PRODUCT1"] ["qty"] * $products ["PRODUCT1"] ["price"];
+      $onionTotal =  $products ["PRODUCT2"] ["qty"] * $products ["PRODUCT2"] ["price"];
       $subTotal += $priceTotal;
+      number_format($riceDiscount, 2);
       
+      $sum += $product["qty"];
       echo $product["item"]. "\t\t " . $product["qty"].$product["qty-type"].  "\t \t " . $product["price"] . "\t \t " . $priceTotal ."\n";
       
-      $grandTotal = $subTotal - $discount;
+      $grandTotal = $subTotal - $riceDiscount;
 
     }
     
     echo "---------------------------------------------------------------\n";
-    echo "SUB-TOTAL:                                       $subTotal \n";
+    echo "SUB-TOTAL:       $sum ITEM(s)                      $subTotal \n\n";
     
-    if ($subTotal >= 1000){
+      //rice discount condition//
+      if ( $product["item"] = "Rice" && $products ["PRODUCT1"] ["qty"] >=5 ){
+        
+        $riceDiscount = $riceTotal * $riceDiscountPercentage;
+        $grandTotal = $subTotal - $riceDiscount;
+        
+        echo   "DISCOUNTS: \t @Rice 10%             \t\t $riceDiscount \n";
+      }
       
-      $discount = $subTotal * $discountPercentage;
-      $grandTotal = $subTotal - $discount;
-      
-      echo "DISCOUNTS:    \t You get a Discount!! \t\t 20% \n \n";
-    }
-    elseif ($subTotal <1000){
-      
-      $discount = $subTotal * $discountPercentage;
-      $grandTotal = $subTotal;
-      
-      echo "DISCOUNTS:   Min. 1000.00 to get 20% discount!   00% \n \n";
-      
-    }
+       //rice discount condition//
+      if ( $product["item"] = "Onion" && $products ["PRODUCT2"] ["qty"] >=5 ){
+        
+        $onionDiscount = $onionTotal * $onionDiscountPercentage;
+        $grandTotal = $subTotal - $riceDiscount - $onionDiscount;
+        
+        echo   "\t\t @Onion 20%               \t $onionDiscount \n\n";
+
+      }
+    
+    
+    
+    
+  
     
     echo "TOTAL:                                           $grandTotal \n \n";
     
     echo "---------------------------------------------------------------\n";
-    echo  "                      Cashier: Jeyson \n";
+    echo  "                       Cashier: Jey \n";
     echo " \t     TRANS#:0000001   ORDER#:0000234 \n";
     echo " \t\t     OR#: 22235454934 \n\n";
     
     
-    date_default_timezone_set("Asia/Kolkata");
+    date_default_timezone_set("Asia/Kuala_Lumpur");
     $date = date("d-m-y h:i:s");
     echo "\t \t     "; echo $date; echo "\n";
    
@@ -101,9 +126,7 @@
     echo "\t THIS RECEIPT IS NOT VALID FOR CLAIM OF INPUT TAX \n"
     
     
+ 
+
+    
 ?>
-
-
-
-
-
